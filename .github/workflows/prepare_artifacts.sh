91b8de976b699d2a10febd10d339dd2b7e95ae2a
Denis S. Soldatov aka General-Beck <general.beck@gmail.com>
2020-03-21 04:08:46 +0300

    remove hashsum per file

--- .github/workflows/prepare_artifacts.sh
+++ .github/workflows/prepare_artifacts.sh
@@ -13,13 +13,10 @@ mkdir -p ./artifacts/
 cd ./target/$1/release/
 echo "_____ Find binary files in target _____"
 find . -maxdepth 1 -type f ! -size 0 -exec grep -IL . "{}" \; | cut -c 3-
-echo "_____ Move binaries to artifacts folder and calculete checksum _____"
+echo "_____ Move binaries to artifacts folder _____"
 for binary in $(find . -maxdepth 1 -type f ! -size 0 -exec grep -IL . "{}" \; | cut -c 3- )
 do
   mv -v $binary ../$dir/../artifacts/$binary
-  rhash --sha3-256 ../$dir/../artifacts/$binary -o ../$dir/../artifacts/$binary.sha3 #Calculating checksum
-  echo "sha3: "$(cat ../$dir/../artifacts/$binary.sha3)
-  #rm -f $binary
 done
 #cd ../$dir/..
 #echo "_____ Clean target dir _____"
